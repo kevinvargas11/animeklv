874c97952d7244a5049f47a063325ff53f90da62
Kevinvargas11 <kevinachacollo11@gmail.com>
2023-12-14 17:11:06 -0400

    Ya funcionan los videos pero algunos no me funcionan

--- lib/UI/screens/videos/videos.dart
+++ lib/UI/screens/videos/videos.dart
@@ -2,8 +2,7 @@ import 'package:animeklv/Data/models/models_anime_list.dart';
 import 'package:animeklv/Data/models/models_anime_video.dart';
 import 'package:animeklv/Domain/peticiones/peticionesd.dart';
 import 'package:flutter/material.dart';
-
-// ... (importaciones previas)
+import 'package:video_player/video_player.dart';
 
 class ScreenVideos extends StatelessWidget {
   final Episode episodeData;
@@ -14,7 +13,7 @@ class ScreenVideos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalles del Episodio'),
+        title: const Text('Detalles del Episodio'),
       ),
       body: Column(
         children: [
@@ -25,7 +24,7 @@ class ScreenVideos extends StatelessWidget {
             future: Gets().getVideoAnime(episodeData.id.toString()),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
+                return const Center(
                   child: CircularProgressIndicator.adaptive(),
                 );
               } else if (snapshot.hasError) {
@@ -33,26 +32,94 @@ class ScreenVideos extends StatelessWidget {
                   child: Text('Error: ${snapshot.error}'),
                 );
               } else if (!snapshot.hasData) {
-                return Center(
+                return const Center(
                   child: Text('No se encontraron datos'),
                 );
               } else {
-                // Datos recibidos con éxito
-                VideoAnime videoAnime = snapshot.data!;
+                final VideoAnime getxVideoAnime = snapshot.data!;
 
-                // Ahora puedes mostrar los datos en tu interfaz de usuario
-                return Column(
-                  children: [
-                    // Muestra los datos de videoAnime como lo desees
-
-                    // Agrega más widgets para mostrar otros detalles si es necesario
-                  ],
+                return VideoFormato(
+                  getxVideoAnime: getxVideoAnime,
                 );
               }
             },
           ),
         ],
       ),
+    );
+  }
+}
+
+class VideoFormato extends StatefulWidget {
+  final VideoAnime getxVideoAnime;
+
+  const VideoFormato({Key? key, required this.getxVideoAnime})
+      : super(key: key);
+
+  @override
+  State<VideoFormato> createState() => _VideoFormatoState();
+}
+
+class _VideoFormatoState extends State<VideoFormato> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(
+        Uri.parse(widget.getxVideoAnime.stream.multi.main.url))
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          // Your video player widget
+          VideoPlayerWidget(controller: _controller),
+
+          // Play/pause button
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _controller.value.isPlaying
+                    ? _controller.pause()
+                    : _controller.play();
+              });
+            },
+            child: Icon(
+              _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+            ),
+          ),
+
+          // Other widgets related to your video
+          Text(widget.getxVideoAnime.info.title),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  const VideoPlayerWidget({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: VideoPlayer(controller),
     );
   }
 }
